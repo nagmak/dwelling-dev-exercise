4a19e0d725b9de5c9edc9bfc8c6d6b65bce743a6
nagmak <nagmakapoor@gmail.com>
2023-07-09 19:09:21 -0400

    updated readme + cleanup

--- src/utils/balanceCheckerUtils.tsx
+++ src/utils/balanceCheckerUtils.tsx
@@ -1,7 +1,11 @@
 /**
- * File to keep utility functions that can be reused for Balance Checker
+ * Utility functions that can be reused for Balance Checker
  */
 
+/**
+ * 
+ * Validation checking for user inputs into the Material UI text field
+ */
 export const handleNumberDisplay = (input: any) => {
     const re = /[a-zA-Z&@*~$()[;\)\:\]\?{\}\-_+=`\^\|\\\\/\"',.!#%<>](.*)/g;
     if (re.test(input)) {
@@ -13,19 +17,27 @@ export const handleNumberDisplay = (input: any) => {
       //If entered value has a length greater than 16 then take only the first 16 digits
       numbersWithoutSpaces = numbersWithoutSpaces.slice(0, 16);
     }
-    console.log(numbersWithoutSpaces)
-        const creditCard: any = [] // Create card as array
-        numbersWithoutSpaces.forEach((val, index) => {
-            if (index % 4 === 0 && index !== 0) creditCard.push(' ') // Add space
-            creditCard.push(val)
-        })
-        return creditCard.join('') // Transform card array to string
+    
+    const creditCard: any = [] // Create card as array
+    numbersWithoutSpaces.forEach((val, index) => {
+      if (index % 4 === 0 && index !== 0) creditCard.push(' ') // Add space
+        creditCard.push(val)
+      })
+    return creditCard.join('') // Transform card array to string
   }
 
+/**
+ * 
+ * Sorts the credit card list by descending order using Balance as the criteria
+ */
   export const sortByBalanceDesc = (list: any) => {
     return list.sort((a: any, b: any) => (b.balance - a.balance))
   };
 
+  /**
+ * 
+ * Fetches computed balance from the API: /api/balance_checker
+ */
   export const getBalance = async(cardValueWithoutSpaces: any) => {
     const res = await fetch(`/api/balance_checker?cardValueWithoutSpaces=${cardValueWithoutSpaces}`).then((res) => {
       if (res.ok) return res.json()
